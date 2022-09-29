f0a9fae1bcd7f915f5025040ad55bd0f8e683de9
Michał Weinert <michal@weinert.io>
2022-09-29 16:24:54 +0200

    Initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+[ -n "${INPUT_DEBUG}" ] && RUNNER_DEBUG=1
+
+log_debug() {
+    if [ -n "${RUNNER_DEBUG}" ]; then
+        echo -e "${*}" | sed 's/^/[debug] /g'
+    fi
+}
+
+echo "${INPUT_CONFIG}" > /tmp/config
+if [ -n "${INPUT_CONTEXT}" ]; then
+    kubectl config set-context "${INPUT_CONTEXT}"
+fi
+log_debug "Current kubectl context: $(kubectl config current-context)"
+
+STERN_ARGS=""
+[ -n "${INPUT_SEARCH_TERM}" ] && STERN_ARGS="${STERN_ARGS} ${INPUT_SEARCH_TERM}"
+[ -n "${INPUT_NAMESPACE}" ] && STERN_ARGS="${STERN_ARGS} --namespace ${INPUT_NAMESPACE}"
+[ -n "${INPUT_SELECTOR}" ] && STERN_ARGS="${STERN_ARGS} --selector ${INPUT_SELECTOR}"
+[ -n "${INPUT_SINCE}" ] && STERN_ARGS="${STERN_ARGS} --since ${INPUT_SINCE}"
+
+if [ -n "${INPUT_UNTIL}" ]; then
+    echo "Running: stern ${STERN_ARGS}, looking for string \"${INPUT_UNTIL}\". Will timeout after ${INPUT_TIMEOUT}."
+    set +e
+    trap "" PIPE
+    timeout "${INPUT_TIMEOUT}" stern ${STERN_ARGS} 2>&1 | sed "/${INPUT_UNTIL}/q"
+    status_codes=("${PIPESTATUS[@]}")
+    if [ "${status_codes[0]}" -eq 143 ]; then
+        echo "Timed out while waiting for \"${INPUT_UNTIL}\"."
+        exit 2
+    elif [ "${status_codes[1]}" -eq 0 ]; then
+        echo "\"${INPUT_UNTIL}\" found."
+        exit 0
+    else
+        exit "$((status_codes[0] + status_codes[1]))"
+    fi
+else
+    echo "Running: stern ${STERN_ARGS}. Will timeout after ${INPUT_TIMEOUT}."
+    set +e
+    timeout "${INPUT_TIMEOUT}" stern ${STERN_ARGS}
+    if [ "${status_code}" -eq 143 ]; then
+        echo "Timed out, as expected."
+        exit 0
+    else
+        exit ${status_code}
+    fi
+fi
