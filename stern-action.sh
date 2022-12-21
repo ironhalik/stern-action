7a5afc3f6ca5a6e330c8a51b5ae1eb566c06f2b1
Michał Weinert <michal@weinert.io>
2022-12-21 14:30:44 +0100

    Shellcheck fixes; updated to kubectl-action-base namespace

--- stern-action.sh
+++ stern-action.sh
@@ -1,6 +1,7 @@
+# shellcheck shell=bash
 # This code is meant to be used with
 # https://github.com/ironhalik/kubectl-action-base
-if [ ! -n "${IS_KUBECTL_ACTION_BASE}" ]; then
+if [ -z "${IS_KUBECTL_ACTION_BASE}" ]; then
     echo "::error:: The script is not meant to be used on it's own."
     exit 1
 fi
@@ -8,7 +9,6 @@ fi
 # kubectl-action specific code
 STERN_ARGS=""
 [ -n "${INPUT_SEARCH_TERM}" ] && STERN_ARGS="${STERN_ARGS} ${INPUT_SEARCH_TERM}"
-[ -n "${INPUT_NAMESPACE}" ] && STERN_ARGS="${STERN_ARGS} --namespace ${INPUT_NAMESPACE}"
 [ -n "${INPUT_SELECTOR}" ] && STERN_ARGS="${STERN_ARGS} --selector ${INPUT_SELECTOR}"
 [ -n "${INPUT_SINCE}" ] && STERN_ARGS="${STERN_ARGS} --since ${INPUT_SINCE}"
 
@@ -16,6 +16,7 @@ if [ -n "${INPUT_UNTIL}" ]; then
     echo "Running: stern ${STERN_ARGS}, looking for string \"${INPUT_UNTIL}\". Will timeout after ${INPUT_TIMEOUT}."
     set +e
     trap "" PIPE
+    # shellcheck disable=SC2086
     timeout "${INPUT_TIMEOUT}" stern ${STERN_ARGS} 2>&1 | sed "/${INPUT_UNTIL}/q"
     status_codes=("${PIPESTATUS[@]}")
     if [ "${status_codes[0]}" -eq 143 ]; then
@@ -30,11 +31,13 @@ if [ -n "${INPUT_UNTIL}" ]; then
 else
     echo "Running: stern ${STERN_ARGS}. Will timeout after ${INPUT_TIMEOUT}."
     set +e
+    # shellcheck disable=SC2086
     timeout "${INPUT_TIMEOUT}" stern ${STERN_ARGS}
+    status_code="${?}"
     if [ "${status_code}" -eq 143 ]; then
         echo "Timed out, as expected."
         exit 0
     else
-        exit ${status_code}
+        exit "${status_code}"
     fi
 fi
